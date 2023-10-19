56d574d4f2721103b1239b0061b4c5bf17011860
Idel <sss_hhh@internet.ru>
2023-10-19 19:57:17 +0600

    Initial commit

---
+++ src/HW4.java
@@ -0,0 +1,27 @@
+public class HW4 {
+    public static void main(String args[]) {
+        int num1 = 5;
+        int num2 = 10;
+        int sum = num1 * num2;
+        System.out.println(sum);
+
+        System.out.println("===========");
+
+        int twenty = 20;
+        double twoHalf = 2.5;
+        double delenie = twenty / twoHalf;
+        System.out.println(delenie);
+
+        System.out.println("===========");
+
+        int a = 15;
+        int b = 9;
+        int perimetr = (15 * 2) + (9 * 2);
+        int ploshad = a * b;
+        System.out.println("Периметр = " + perimetr + " Площадь = " + ploshad);
+
+        System.out.println("===========");
+
+
+    }
+}
